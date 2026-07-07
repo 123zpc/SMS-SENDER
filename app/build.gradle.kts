626afe5ff30b1dc409cba82d618c9ec0ec30e345
123zpc <3130996969@qq.com>
2026-07-07 09:55:48 +0800

    Use available stable Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.smsagent"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.smsagent"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
